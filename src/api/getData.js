5ae6e11a11d93e88b67217fcb1eb415e77893e21
jean <jean.morell@datamaran.com>
2022-12-02 00:03:41 +0100

    fetch Data from the api to send to the cards components

---
+++ src/api/getData.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const api = "https://api.staging.bsport.io/api/v1"
+const headers = {Authorization: "Token f18688960a8942c83d238b04e88389ac126bf55c"}
+
+const getCardData = async (date) => {
+    async function getOffers() {
+        const res = await axios.get(api+'/offer', {params: { company: 6, min_date: date, max_date: date }, headers})
+        return res.data.results
+    }
+    const offer = await getOffers()
+
+    let activityList = offer.map(obj => obj.meta_activity)
+    activityList = [...new Set(activityList)]
+
+    async function getActivities() {
+        const res = await axios.get(api+'/meta-activity/?company=6&id__in='+ activityList.join(","), {headers})
+        return res.data.results
+    }
+    const activities = await getActivities()
+
+    let coachList = offer.map(obj => obj.coach)
+    coachList = [...new Set(coachList)]
+
+    async function getCoach() {
+        const res = await axios.get(api+'/coach/?company=6&id__in='+ coachList.join(","), {headers})
+        return res.data.results
+    }
+
+    const coach = await getCoach()
+
+    let cards = []
+    offer.forEach(off => {
+        console.log(coach)
+        let obj = {}
+        obj.id = off.id
+        obj.hour_start = new Date(off.date_start).getHours()
+        obj.min_start = new Date(off.date_start).getMinutes()
+        obj.duration = off.duration_minute
+        obj.coach = coach.find(c => c.id === off.coach).user
+        obj.name = activities.find(a => a.id === off.meta_activity).name
+        cards.push(obj)
+    })
+
+    return cards
+}
+
+export default getCardData  
--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,19 +1,34 @@
 import * as React from 'react';
 import { Divider, List } from '@mui/material';
+import getCardData from '../api/getData';
 
-export default function Schedule() {
+
+export default function Schedule(props) {
     const listHours = Array.from({length:25},(v,k)=>k)
+    const [cardData, setCardData] = React.useState([])
+    const date = props.date
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const res = await getCardData(date)
+            setCardData(res)
+        }
+        fetchData()
+    }, [date])
+    console.log(cardData)
+
+    
+
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <List 
         sx={{ width: '80%'}}>
             {listHours.map((hour) => (
-                <li>
+                <li key={hour}>
                     <div style={{height: '160px', textAlign: 'left', paddingTop: '5px'}}>{hour}</div>
                     <Divider />
                 </li>
             ))}
-
         </List> 
     </div>
   );
